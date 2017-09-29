ebf505f32cce847846195597fd4969e9f07fe1d5
Clint Ferrin <clint.ferrin@gmail.com>
2017-09-29 11:40:24 -0600

    Separates problems correctly.

--- lab1.cpp
+++ lab1.cpp
@@ -5,8 +5,10 @@
 
 arma::mat read_pgm(std::string fname){
 	unsigned char d;
-	char header [100], *ptr;
     std::ifstream infile;
+    std::stringstream ss;
+    std::string inputLine = "";
+
 	infile.open(fname, std::ios::binary);
 
 	if (!infile) {
@@ -14,35 +16,32 @@ arma::mat read_pgm(std::string fname){
 		exit(1);
 	}
 
-	infile.getline(header,100,'\n');
-	if((header[0]!='P') || header[1]!='5')   /* 'P5' Formay */
-	{
-        std::cout << "Image <" << fname << "> is not in binary PGM 'P5' format." << std::endl;
-		exit(1);
-	}
+    getline(infile,inputLine);
 
-	infile.getline(header,100,'\n');
-	while(header[0]=='#')
-		infile.getline(header,100,'\n');
+    if(inputLine.compare("P5") != 0){
+        std::cerr << "Image version error" << std::endl; 
+        exit(1);
+    }
+
+	getline(infile,inputLine);
+	while(inputLine[0]=='#')
+	    getline(infile,inputLine);
 
     int M,N;
-	M=strtol(header,&ptr,0);
-	N=atoi(ptr);
+    ss.str(inputLine);
+    ss >> N >> M;
+    getline(infile,inputLine);
 
-    std::cout << M << " " << N << std::endl;
-
-	infile.getline(header,100,'\n');
-
-	// Q=strtol(header,&ptr,0);
-
+    
     arma::mat fimage(M,N);
 
-	for(int i=0; i<N; i++) {
-		for(int j=0; j<M; j++) {
-			d = infile.get();
-			fimage(i,j)= (int)d;
-		}
-	}
+    for(int i=0; i<M; i++) {
+        for(int j=0; j<N; j++) {
+            d = infile.get();
+            fimage(i,j)= (int)d;
+        }
+    }
+
     return fimage;
 }
 void write_pgm(arma::mat image, std::string name) {
@@ -79,28 +78,6 @@ arma::mat make_H_1() {
     return 1/double(81)*mat;
 }
 
-arma::mat conv2d(arma::mat X, arma::mat H) {
-    arma::mat Y(X.n_rows,X.n_cols);
-    for (unsigned long long i = 0; i < X.n_rows; i++) {	
-        for (unsigned long long j = 0; j < X.n_cols; j++) {
-            int sum1 = 0;
-            int sum2 = 0;
-            
-            // calculate convolution for each pixel
-            for (int k = 0; k < 3; k++) {
-                for (int l = 0; l < 3; l++) {
-                    if ((i+k-1 > 0 && j+l-1 > 0) && (i+k-1 < X.n_rows && j+l-1 < X.n_cols)) {
-                        sum1 += X(i+k-1,j+l) * H(k,l);
-                        sum2 += X(i+k-1,j+l) * H(k,l);
-                    }
-                }
-            }
-            Y(i,j) = sqrt(sum1*sum1 + sum2*sum2);
-        }
-    }  
-    return Y;
-}
-
 arma::mat add_row_border(arma::mat X, int n) {
     for (int i = 0; i < n; i++) {
         arma::mat Z_row(1,X.n_cols); Z_row.fill(0); 
@@ -119,7 +96,7 @@ arma::mat add_col_border(arma::mat X, int n) {
     return X;
 }
 
-arma::mat conv2d_comp(arma::mat X, arma::mat H) {
+arma::mat conv2d(arma::mat X, arma::mat H) {
     arma::mat Y(X.n_rows+H.n_rows-1,X.n_cols+H.n_cols-1);
     X = add_row_border(X,H.n_rows-1);
     X = add_col_border(X,H.n_cols-1);
@@ -138,6 +115,7 @@ arma::mat conv2d_comp(arma::mat X, arma::mat H) {
     return Y;
 }
 
+
 arma::mat threshold_mat(arma::mat mat, int min, int max){
      for (unsigned long long i = 0; i < mat.n_rows; i++) {
          for (unsigned long long j = 0; j < mat.n_cols; j++){
@@ -149,33 +127,110 @@ arma::mat threshold_mat(arma::mat mat, int min, int max){
 }
 
 arma::mat reduce_dimension(arma::mat X, int rows, int cols){
-    int remove_rows = (X.n_rows - rows)/2;
-    int remove_cols= (X.n_cols- cols)/2;
-    std::cout << remove_cols << std::endl;
-    std::cout << remove_rows << std::endl;
+    int remove_rows = (X.n_rows - rows);
+    int remove_cols= (X.n_cols- cols);
 
-    for (int i = 0; i < remove_rows; i++) {
-        X.shed_col(0);
-    }
-    for (unsigned long long i = 0; i < X.n_rows-rows; i++) {
-       X.shed_col(X.n_rows) ;
-    }
+    X.shed_cols(0,remove_cols/2-1);
+    remove_cols= (X.n_cols- cols);
+    X.shed_cols(X.n_cols-remove_cols,X.n_cols-1);
 
-    for (int i = 0; i < remove_cols; i++) {
-        X.shed_row(0);
-    }
-    for (unsigned long long i = 0; i < X.n_cols-cols; i++) {
-       X.shed_row(X.n_cols) ;
-    }
+    X.shed_rows(0,remove_rows/2-1);
+    remove_rows= (X.n_rows- rows);
+    X.shed_rows(X.n_rows-remove_rows,X.n_rows-1);
     return X;
 }
 
+arma::mat conv2d_aspects(arma::mat X, arma::mat H){
+    arma::mat Y = conv2d(X,H); 
+    Y = reduce_dimension(Y,X.n_rows,X.n_cols);
+    return Y;
+}
+
+arma::mat make_S_1(){
+    arma::mat mat(3,3);
+
+    double data[9] = 
+    { 1,0,-1,
+      2,0,-2,
+      1,0,-1 };
+
+    for (unsigned long long i = 0; i < mat.n_rows; i++) {
+        for (unsigned long long j = 0; j < mat.n_cols; j++) {
+            mat(i,j) = data[i*mat.n_rows+j];
+        }
+    }
+
+    return mat;
+}
+
+arma::mat make_S_2(){
+    arma::mat mat(3,3);
+
+    double data[9] = 
+    { -1,-2,-1,
+       0, 0, 0,
+       1, 2, 1  };
+
+    for (unsigned long long i = 0; i < mat.n_rows; i++) {
+        for (unsigned long long j = 0; j < mat.n_cols; j++) {
+            mat(i,j) = data[i*mat.n_rows+j];
+        }
+    }
+
+    return mat;
+}
+
+void problem1(){
+    arma::mat F, H_1,Y;
+    F = read_pgm("../source/image.pgm");
+
+    H_1 = make_H_1();
+
+    Y = conv2d_aspects(F, H_1);
+    Y = threshold_mat(Y,0,255);
+    write_pgm(Y,"problem1.pgm");
+}
+
+void problem2(){
+    arma::mat F, S_1, S_2, G_1, G_2, Y, Y_v, Y_h;
+    F = read_pgm("../source/image.pgm");
+
+    S_1 = make_S_1();
+    S_2 = make_S_2();
+
+    // vertical convolution
+    Y_v = conv2d_aspects(F,S_1);
+    Y_v = threshold_mat(Y_v,0,255);
+    Y_h = conv2d_aspects(F,S_2);
+    Y_h = threshold_mat(Y_h,0,255);
+
+
+    G_1 = conv2d_aspects(F,S_1);
+    G_2 = conv2d_aspects(F,S_2);
+    Y = arma::abs(G_1) + arma::abs(G_2);
+
+    write_pgm(Y,"problem2.pgm");
+    write_pgm(Y_v,"problem2_ver.pgm");
+    write_pgm(Y_h,"problem2_hor.pgm");
+}
+
+void problem3(){
+    arma::mat F, H, Y;
+    int min, max;
+    F = read_pgm("../source/image.pgm");
+    H = read_pgm("../source/filter_final.pgm");
+    
+    min = arma::min(arma::min(H));
+    max = arma::max(arma::max(H));
+
+    H = H-min;
+    H = H*255/max; 
+
+    Y = conv2d_aspects(F,H);
+
+    write_pgm(Y,"problem3.pgm");
+}
+
 int main() {
-    arma::mat image(read_pgm("../source/image.pgm"));
-    // arma::mat H(2,2);
-    // H = make_H_1();
-    // arma::mat Y = conv2d_comp(image,H);
-    // Y = threshold_mat(Y,0,255);
-    // Y = reduce_dimension(Y,image.n_rows,image.n_cols);
-    write_pgm(image,"output.pgm");
+    problem3();
 }
